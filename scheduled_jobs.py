6cb008fd8b8f25cb2d01740a5f201e9c2b8990fc
Ned Reid <Ned.Reid@softwire.com>
2025-09-03 12:14:31 +0100

    Change to use local timezones

--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -2,7 +2,6 @@ from data.database import save_order, get_all_orders
 from products import create_product_download
 from apscheduler.schedulers.background import BackgroundScheduler
 import requests
-from datetime import timezone
 
 def initialise_scheduled_jobs(app):
     scheduler = BackgroundScheduler()
@@ -26,7 +25,7 @@ def process_orders(app):
         payload = {
             "product": order.product,
             "customer": order.customer,
-            "date": order.date_placed.astimezone(timezone.utc).isoformat(),
+            "date": order.date_placed_local.isoformat(),
         }
 
         response = requests.post(
